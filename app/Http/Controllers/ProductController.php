23ce765020576a8795f10acb57302a9248aec5a8
Nashinotane <74orisssa84@gmail.com>
2024-02-12 03:53:25 +0900

    変更

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -27,7 +27,9 @@ class ProductController extends Controller
      */
     public function create()
     {
-        return view('products.create');
+        $categories = Category::all();
+  
+         return view('products.create', compact('categories'));
     }
 
     /**
@@ -42,6 +44,7 @@ class ProductController extends Controller
          $product->name = $request->input('name');
          $product->description = $request->input('description');
          $product->price = $request->input('price');
+         $product->category_id = $request->input('category_id');
          $product->save();
  
          return to_route('products.index');
@@ -66,7 +69,9 @@ class ProductController extends Controller
      */
     public function edit(Product $product)
     {
-        return view('products.edit', compact('product'));
+        $categories = Category::all();
+  
+         return view('products.edit', compact('product', 'categories'));
     }
 
     /**
@@ -81,6 +86,7 @@ class ProductController extends Controller
         $product->name = $request->input('name');
          $product->description = $request->input('description');
          $product->price = $request->input('price');
+         $product->category_id = $request->input('category_id');
          $product->update();
  
          return to_route('products.index');
