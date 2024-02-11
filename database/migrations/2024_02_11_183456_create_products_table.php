8ce85a401c1c1a7176a245996c6225378202721c
Nashinotane <74orisssa84@gmail.com>
2024-02-12 03:18:00 +0900

    店舗モデル作成

--- database/migrations/2024_02_11_183456_create_products_table.php
+++ database/migrations/2024_02_11_183456_create_products_table.php
@@ -15,6 +15,18 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->integer('max_price')->unsigned();
+            $table->integer('min_price')->unsigned();
+            $table->datetime('close_time');
+            $table->datetime('open_time');
+            $table->string('郵便番号');
+            $table->string('住所');
+            $table->string('電話番号');
+            $table->date('regular holiday');
+            $table->date('registration_date');
+            $table->date('update_date');
             $table->timestamps();
         });
     }
