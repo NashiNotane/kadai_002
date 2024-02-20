0109f2bf7b5ef16dbc8027afefd549a852adf31d
Nashinotane <74orisssa84@gmail.com>
2024-02-21 05:02:16 +0900

    作成

--- database/seeders/ShopSeeder.php
+++ database/seeders/ShopSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Shop;
 
 class ShopSeeder extends Seeder
 {
@@ -14,6 +15,6 @@ class ShopSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Shop::factory()->count(20)->create()
     }
 }
